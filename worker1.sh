c34d71ff453e0e74b4ec3d794db2dd98d24a0d94
kaushikyasaswy <kaus@seas.upenn.edu>
2015-05-10 14:20:44 -0400

    mapreduce framework

--- worker1.sh
+++
@@ -1,4 +0,0 @@
-rm -rf /home/cis455/Documents/tomcat2/webapps/worker
-rm /home/cis455/Documents/tomcat2/webapps/worker.war
-cp worker.war /home/cis455/Documents/tomcat2/webapps
-sh /home/cis455/Documents/tomcat2/bin/catalina.sh run
--- worker2.sh
+++
@@ -1,4 +0,0 @@
-rm -rf /home/cis455/Documents/tomcat3/webapps/worker
-rm /home/cis455/Documents/tomcat3/webapps/worker.war
-cp worker.war /home/cis455/Documents/tomcat3/webapps
-sh /home/cis455/Documents/tomcat3/bin/catalina.sh run
